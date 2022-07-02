ef6b5e06c4d45eb3311f3faa210f4b5819e0c66b
Rodrigo Martinez <rodrigomartinez1477@gmail.com>
2022-07-01 19:01:22 -0700

    added navbar js and moved code to correct files

--- UCMERCED-SHPE-WEBSITE/src/Home/Home.js
+++ UCMERCED-SHPE-WEBSITE/src/Home/Home.js
@@ -1,29 +1,12 @@
 import React from "react";
-import Logo from "../../../assets/SHPE_logo_horiz_UC Merced_CMYK.png";
+import "./Home.css";
+import Navbar from "../Navbar/Navbar";
 
 function Home() {
   return (
-    <nav class="navbar">
-      <div class="logo">
-        <a href="www.google.com">
-          <img src={Logo} alt="Logo" />
-        </a>
-      </div>
-      <ul class="nav-links">
-        <li class="link-item">
-          <a href="www.google.com">Home</a>
-        </li>
-        <li class="link-item">
-          <a href="www.google.com">About Us</a>
-        </li>
-        <li class="link-item">
-          <a href="www.google.com">Alumni</a>
-        </li>
-        <li class="link-item">
-          <a href="www.google.com">Join</a>
-        </li>
-      </ul>
-    </nav>
+    <div className="parent-container">
+      <Navbar />
+    </div>
   );
 }
 
---
+++ UCMERCED-SHPE-WEBSITE/src/Navbar/Navbar.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./Navbar.css";
+import Logo from "../assets/SHPE_logo_horiz_UC Merced_CMYK.png";
+
+function Navbar() {
+  return (
+    <nav class="navbar">
+      <div class="logo">
+        <a href="www.google.com">
+          <img class="primary-logo" src={Logo} alt="Logo" />
+        </a>
+      </div>
+      <ul class="nav-links">
+        <li class="link-item">
+          <a href="www.google.com">Home</a>
+        </li>
+        <li class="link-item">
+          <a href="www.google.com">About Us</a>
+        </li>
+        <li class="link-item">
+          <a href="www.google.com">Alumni</a>
+        </li>
+        <li class="link-item">
+          <a href="www.google.com">Join</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
