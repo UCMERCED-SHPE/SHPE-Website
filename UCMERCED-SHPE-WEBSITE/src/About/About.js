ecc530f59aac3ab6b43c25b71ab8bdcb32391d34
Rodrigo Martinez <rodrigomartinez1477@gmail.com>
2022-07-03 22:53:36 -0700

    Added files and flxed routing in navbar

---
+++ UCMERCED-SHPE-WEBSITE/src/About/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <h4>This is the About Us page</h4>
+    </div>
+  );
+}
+
+export default About;
--- UCMERCED-SHPE-WEBSITE/src/Navbar/Navbar.js
+++ UCMERCED-SHPE-WEBSITE/src/Navbar/Navbar.js
@@ -2,27 +2,30 @@ import "./Navbar.css";
 import React from "react";
 //Currently looking into Navlinks from react-router-dom
 import Logo from "../assets/SHPE_logo_horiz_UC Merced_DKBG.png";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     <nav class="navbar">
       <div class="logo">
         <a href="www.google.com">
-          <img class="primary-logo" src={Logo} alt="Logo" />
+          <Link to="/">
+            <img class="primary-logo" src={Logo} alt="Logo" />
+          </Link>
         </a>
       </div>
       <ul class="nav-links">
         <li class="link-item">
-          <a href="www.google.com">Home</a>
+          <Link to="/">Home</Link>
         </li>
         <li class="link-item">
-          <a href="www.google.com">About Us</a>
+          <Link to="/about">About Us</Link>
         </li>
         <li class="link-item">
-          <a href="www.google.com">Alumni</a>
+          <Link to="/join">Join</Link>
         </li>
         <li class="link-item">
-          <a href="www.google.com">Join</a>
+          <Link to="/contact">Contact Us</Link>
         </li>
       </ul>
     </nav>
