6d4a449dfb06a5f84921d0d294fea469e96dccfb
vcortez4 <vcortez4@ucmerced.edu>
2022-07-18 20:29:44 -0700

    created 2 rough draft containers for board members

--- UCMERCED-SHPE-WEBSITE/src/EBoard/EBoard.js
+++ UCMERCED-SHPE-WEBSITE/src/EBoard/EBoard.js
@@ -8,9 +8,22 @@ function EBoard() {
       <div className="Title">
         <h4 className="text"> Executive Board 2022</h4>
       </div>
+
+  
       <div className="mem1-container">
         <b1 className = "mem1"> Board Member one</b1>
       </div>
+
+      <div className="mem2-container">
+
+      </div>
+
+      <div className="mem3-container">
+
+      </div>
+
+
+      
     </div>
   );
 }
