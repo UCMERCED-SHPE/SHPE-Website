c23dcfb56b71add889e7ca0ff3c2b860e4e10568
vcortez4 <vcortez4@ucmerced.edu>
2022-07-18 21:38:40 -0700

    created text for board mem names

--- UCMERCED-SHPE-WEBSITE/src/EBoard/EBoard.js
+++ UCMERCED-SHPE-WEBSITE/src/EBoard/EBoard.js
@@ -9,50 +9,48 @@ function EBoard() {
         <h4 className="text"> Executive Board 2022</h4>
       </div>
 
-  
       <div className="mem1-container">
-        <b1 className = "mem1"> Board Member one</b1>
       </div>
+      <h4 className="b1">Board Member 1</h4>
 
       <div className="mem2-container">
-
       </div>
+      <h4 className="b2">Board Member 2</h4>
+
 
       <div className="mem3-container">
-
       </div>
+      <h4 className="b3">Board Member 3</h4>
 
-      
-
+    
       <div className="mem4-container">
-
       </div>
+      <h4 className="b4">Board Member 4</h4>
 
 
       <div className="mem5-container">
-
       </div>
+      <h4 className="b5">Board Member 5</h4>
 
 
       <div className="mem6-container">
-
       </div>
-
+      <h4 className="b6">Board Member 6</h4>
 
 
       <div className="mem7-container">
-
       </div>
+      <h4 className="b7">Board Member 7</h4>
 
 
       <div className="mem8-container">
-
       </div>
+      <h4 className="b8">Board Member 8</h4>
 
 
       <div className="mem9-container">
-
       </div>
+      <h4 className="b9">Board Member 9</h4>
 
 
     </div>
