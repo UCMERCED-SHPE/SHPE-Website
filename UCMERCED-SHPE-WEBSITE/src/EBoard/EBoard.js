915a3fa45143581cd880d93e1d01eb1b113f4758
vcortez4 <vcortez4@ucmerced.edu>
2022-07-20 18:22:24 -0700

    made ex image to fit

--- UCMERCED-SHPE-WEBSITE/src/EBoard/EBoard.js
+++ UCMERCED-SHPE-WEBSITE/src/EBoard/EBoard.js
@@ -10,45 +10,54 @@ function EBoard() {
       </div>
 
       <div className="mem1-container">
+        <img src="https://th.bing.com/th/id/R.637e6314b636f28a08577f65550c9f3a?rik=uE%2fIHMZV6KZCpw&riu=http%3a%2f%2fwww.clipartbest.com%2fcliparts%2fyik%2fgAd%2fyikgAdr6T.jpg&ehk=izbxCAjnfmMRHezYkA6F%2fpoKVSZn5TUwV7WVcKsYIN4%3d&risl=&pid=ImgRaw&r=0" alt="example img"/>
       </div>
       <h4 className="b1">Board Member 1</h4>
 
       <div className="mem2-container">
+      <img src="https://th.bing.com/th/id/R.637e6314b636f28a08577f65550c9f3a?rik=uE%2fIHMZV6KZCpw&riu=http%3a%2f%2fwww.clipartbest.com%2fcliparts%2fyik%2fgAd%2fyikgAdr6T.jpg&ehk=izbxCAjnfmMRHezYkA6F%2fpoKVSZn5TUwV7WVcKsYIN4%3d&risl=&pid=ImgRaw&r=0" alt="example img"/>
       </div>
       <h4 className="b2">Board Member 2</h4>
 
 
       <div className="mem3-container">
+      <img src="https://th.bing.com/th/id/R.637e6314b636f28a08577f65550c9f3a?rik=uE%2fIHMZV6KZCpw&riu=http%3a%2f%2fwww.clipartbest.com%2fcliparts%2fyik%2fgAd%2fyikgAdr6T.jpg&ehk=izbxCAjnfmMRHezYkA6F%2fpoKVSZn5TUwV7WVcKsYIN4%3d&risl=&pid=ImgRaw&r=0" alt="example img"/>
       </div>
       <h4 className="b3">Board Member 3</h4>
 
     
       <div className="mem4-container">
+      <img src="https://th.bing.com/th/id/R.637e6314b636f28a08577f65550c9f3a?rik=uE%2fIHMZV6KZCpw&riu=http%3a%2f%2fwww.clipartbest.com%2fcliparts%2fyik%2fgAd%2fyikgAdr6T.jpg&ehk=izbxCAjnfmMRHezYkA6F%2fpoKVSZn5TUwV7WVcKsYIN4%3d&risl=&pid=ImgRaw&r=0" alt="example img"/>
       </div>
       <h4 className="b4">Board Member 4</h4>
 
 
       <div className="mem5-container">
+      <img src="https://th.bing.com/th/id/R.637e6314b636f28a08577f65550c9f3a?rik=uE%2fIHMZV6KZCpw&riu=http%3a%2f%2fwww.clipartbest.com%2fcliparts%2fyik%2fgAd%2fyikgAdr6T.jpg&ehk=izbxCAjnfmMRHezYkA6F%2fpoKVSZn5TUwV7WVcKsYIN4%3d&risl=&pid=ImgRaw&r=0" alt="example img"/>
       </div>
       <h4 className="b5">Board Member 5</h4>
 
 
       <div className="mem6-container">
+      <img src="https://th.bing.com/th/id/R.637e6314b636f28a08577f65550c9f3a?rik=uE%2fIHMZV6KZCpw&riu=http%3a%2f%2fwww.clipartbest.com%2fcliparts%2fyik%2fgAd%2fyikgAdr6T.jpg&ehk=izbxCAjnfmMRHezYkA6F%2fpoKVSZn5TUwV7WVcKsYIN4%3d&risl=&pid=ImgRaw&r=0" alt="example img"/>
       </div>
       <h4 className="b6">Board Member 6</h4>
 
 
       <div className="mem7-container">
+      <img src="https://th.bing.com/th/id/R.637e6314b636f28a08577f65550c9f3a?rik=uE%2fIHMZV6KZCpw&riu=http%3a%2f%2fwww.clipartbest.com%2fcliparts%2fyik%2fgAd%2fyikgAdr6T.jpg&ehk=izbxCAjnfmMRHezYkA6F%2fpoKVSZn5TUwV7WVcKsYIN4%3d&risl=&pid=ImgRaw&r=0" alt="example img"/>
       </div>
       <h4 className="b7">Board Member 7</h4>
 
 
       <div className="mem8-container">
+      <img src="https://th.bing.com/th/id/R.637e6314b636f28a08577f65550c9f3a?rik=uE%2fIHMZV6KZCpw&riu=http%3a%2f%2fwww.clipartbest.com%2fcliparts%2fyik%2fgAd%2fyikgAdr6T.jpg&ehk=izbxCAjnfmMRHezYkA6F%2fpoKVSZn5TUwV7WVcKsYIN4%3d&risl=&pid=ImgRaw&r=0" alt="example img"/>
       </div>
       <h4 className="b8">Board Member 8</h4>
 
 
       <div className="mem9-container">
+      <img src="https://th.bing.com/th/id/R.637e6314b636f28a08577f65550c9f3a?rik=uE%2fIHMZV6KZCpw&riu=http%3a%2f%2fwww.clipartbest.com%2fcliparts%2fyik%2fgAd%2fyikgAdr6T.jpg&ehk=izbxCAjnfmMRHezYkA6F%2fpoKVSZn5TUwV7WVcKsYIN4%3d&risl=&pid=ImgRaw&r=0" alt="example img"/>
       </div>
       <h4 className="b9">Board Member 9</h4>
 
