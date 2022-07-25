2a7318864fcfcd4b8c4f1cc1116626deb7ed1a2b
vcortez4 <vcortez4@ucmerced.edu>
2022-07-24 17:59:53 -0700

    better layout for board members

--- UCMERCED-SHPE-WEBSITE/src/EBoard/EBoard.js
+++ UCMERCED-SHPE-WEBSITE/src/EBoard/EBoard.js
@@ -9,10 +9,15 @@ function EBoard() {
         <h4 className="text"> Executive Board 2022</h4>
       </div>
 
-      <div className="mem1-container">
-        <img src="https://th.bing.com/th/id/R.637e6314b636f28a08577f65550c9f3a?rik=uE%2fIHMZV6KZCpw&riu=http%3a%2f%2fwww.clipartbest.com%2fcliparts%2fyik%2fgAd%2fyikgAdr6T.jpg&ehk=izbxCAjnfmMRHezYkA6F%2fpoKVSZn5TUwV7WVcKsYIN4%3d&risl=&pid=ImgRaw&r=0" alt="example img"/>
-      </div>
-      <h4 className="b1">Board Member 1</h4>
+    <div className="boarder1">
+      <div className="decor1"></div>
+      <p className="info1">I like food, dogs, fighting, sometimes cooking, drinking, hang with friends sometimes
+      blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah</p>
+    </div>
+    <div className="mem1-container">
+        <img src="https://th.bing.com/th/id/R.4b8ae6534962afba293dca06b718b5df?rik=htS%2bEp2O%2fNwT4Q&riu=http%3a%2f%2fwww.pngmart.com%2ffiles%2f4%2fCute-Cartoon-PNG-Transparent.png&ehk=brf9GkGR2T%2b3zNn5UMkcl4UpZgEauvzVpjGIsvNEzUI%3d&risl=&pid=ImgRaw&r=0" alt="example img"/>
+    </div>
+    <h4 className="b1">Board Member 1</h4>
 
       <div className="mem2-container">
       <img src="https://th.bing.com/th/id/R.637e6314b636f28a08577f65550c9f3a?rik=uE%2fIHMZV6KZCpw&riu=http%3a%2f%2fwww.clipartbest.com%2fcliparts%2fyik%2fgAd%2fyikgAdr6T.jpg&ehk=izbxCAjnfmMRHezYkA6F%2fpoKVSZn5TUwV7WVcKsYIN4%3d&risl=&pid=ImgRaw&r=0" alt="example img"/>
