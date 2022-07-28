05e6e92e0c2cae4d6fbeaf7e3b161fb2373b281b
vcortez4 <vcortez4@ucmerced.edu>
2022-07-27 18:00:41 -0700

    made more containers and creating message

--- UCMERCED-SHPE-WEBSITE/src/EBoard/EBoard.js
+++ UCMERCED-SHPE-WEBSITE/src/EBoard/EBoard.js
@@ -5,66 +5,91 @@ import Navbar from "../Navbar/Navbar";
 function EBoard() {
   return (
     <div className="Parent">
+
       <div className="Title">
         <h4 className="text"> Executive Board 2022</h4>
       </div>
 
-    <div className="boarder1">
-      <div className="decor1"></div>
-      <p className="info1">I like food, dogs, fighting, sometimes cooking, drinking, hang with friends sometimes
-      blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah</p>
-    </div>
-    <div className="mem1-container">
-        <img src="https://th.bing.com/th/id/R.4b8ae6534962afba293dca06b718b5df?rik=htS%2bEp2O%2fNwT4Q&riu=http%3a%2f%2fwww.pngmart.com%2ffiles%2f4%2fCute-Cartoon-PNG-Transparent.png&ehk=brf9GkGR2T%2b3zNn5UMkcl4UpZgEauvzVpjGIsvNEzUI%3d&risl=&pid=ImgRaw&r=0" alt="example img"/>
-    </div>
-    <h4 className="b1">Board Member 1</h4>
-
-      <div className="mem2-container">
-      <img src="https://th.bing.com/th/id/R.637e6314b636f28a08577f65550c9f3a?rik=uE%2fIHMZV6KZCpw&riu=http%3a%2f%2fwww.clipartbest.com%2fcliparts%2fyik%2fgAd%2fyikgAdr6T.jpg&ehk=izbxCAjnfmMRHezYkA6F%2fpoKVSZn5TUwV7WVcKsYIN4%3d&risl=&pid=ImgRaw&r=0" alt="example img"/>
+      <div className="intro-container">
+        <img src="https://www.mccarthy.com/sites/default/files/project-images/UC%20Merced%20Science%20and%20Engineering%20Building-005.jpg" alt="school"/>
       </div>
-      <h4 className="b2">Board Member 2</h4>
+      <div className="m"></div>
 
 
-      <div className="mem3-container">
-      <img src="https://th.bing.com/th/id/R.637e6314b636f28a08577f65550c9f3a?rik=uE%2fIHMZV6KZCpw&riu=http%3a%2f%2fwww.clipartbest.com%2fcliparts%2fyik%2fgAd%2fyikgAdr6T.jpg&ehk=izbxCAjnfmMRHezYkA6F%2fpoKVSZn5TUwV7WVcKsYIN4%3d&risl=&pid=ImgRaw&r=0" alt="example img"/>
+
+
+
+
+
+      <div className="boarder1">
+        <div className="decor1"></div>
+        <p className="info1">I like food, dogs, fighting, sometimes cooking, drinking, hang with friends sometimes
+        blah blah blah blah blah blah blah blah blah blah blah blah blah</p>
       </div>
-      <h4 className="b3">Board Member 3</h4>
+      <div className="mem1-container">
+          <img src="https://th.bing.com/th/id/R.4b8ae6534962afba293dca06b718b5df?rik=htS%2bEp2O%2fNwT4Q&riu=http%3a%2f%2fwww.pngmart.com%2ffiles%2f4%2fCute-Cartoon-PNG-Transparent.png&ehk=brf9GkGR2T%2b3zNn5UMkcl4UpZgEauvzVpjGIsvNEzUI%3d&risl=&pid=ImgRaw&r=0" alt="example img"/>
+      </div>
+      <h4 className="b1">Board Member 1</h4>
+
 
     
-      <div className="mem4-container">
-      <img src="https://th.bing.com/th/id/R.637e6314b636f28a08577f65550c9f3a?rik=uE%2fIHMZV6KZCpw&riu=http%3a%2f%2fwww.clipartbest.com%2fcliparts%2fyik%2fgAd%2fyikgAdr6T.jpg&ehk=izbxCAjnfmMRHezYkA6F%2fpoKVSZn5TUwV7WVcKsYIN4%3d&risl=&pid=ImgRaw&r=0" alt="example img"/>
+      <div className="boarder2">
+        <div className="decor2"></div>
+        <p className="info2">I like food, dogs, fighting, sometimes cooking, drinking, hang with friends sometimes
+         blah blah blah blah</p>
       </div>
-      <h4 className="b4">Board Member 4</h4>
+      <div className="mem2-container">
+        <img src="https://th.bing.com/th/id/R.4b8ae6534962afba293dca06b718b5df?rik=htS%2bEp2O%2fNwT4Q&riu=http%3a%2f%2fwww.pngmart.com%2ffiles%2f4%2fCute-Cartoon-PNG-Transparent.png&ehk=brf9GkGR2T%2b3zNn5UMkcl4UpZgEauvzVpjGIsvNEzUI%3d&risl=&pid=ImgRaw&r=0" alt="example img"/>
+      </div>
+        <h4 className="b2">Board Member 2</h4>
+
+      <div className="boarder3">
+        <div className="decor3"></div>
+        <p className="info3">I like food, dogs, fighting, sometimes cooking, drinking, hang with friends sometimes
+         blah blah blah blah</p>
+      </div>
+      <div className="mem3-container">
+        <img src="https://th.bing.com/th/id/R.4b8ae6534962afba293dca06b718b5df?rik=htS%2bEp2O%2fNwT4Q&riu=http%3a%2f%2fwww.pngmart.com%2ffiles%2f4%2fCute-Cartoon-PNG-Transparent.png&ehk=brf9GkGR2T%2b3zNn5UMkcl4UpZgEauvzVpjGIsvNEzUI%3d&risl=&pid=ImgRaw&r=0" alt="example img"/>
+      </div>
+        <h4 className="b3">Board Member 3</h4>
 
 
+
+
+      <div className="boarder4">
+        <div className="decor4"></div>
+        <p className="info4">I like food, dogs, fighting, sometimes cooking, drinking, hang with friends sometimes
+         blah blah blah blah</p>
+      </div>    
+      <div className="mem4-container">
+        <img src="https://th.bing.com/th/id/R.4b8ae6534962afba293dca06b718b5df?rik=htS%2bEp2O%2fNwT4Q&riu=http%3a%2f%2fwww.pngmart.com%2ffiles%2f4%2fCute-Cartoon-PNG-Transparent.png&ehk=brf9GkGR2T%2b3zNn5UMkcl4UpZgEauvzVpjGIsvNEzUI%3d&risl=&pid=ImgRaw&r=0" alt="example img"/>
+      </div>
+        <h4 className="b4">Board Member 4</h4>
+
+
+
+
+      <div className="boarder5">
+        <div className="decor5"></div>
+        <p className="info5">I like food, dogs, fighting, sometimes cooking, drinking, hang with friends sometimes
+         blah blah blah blah</p>
+      </div> 
       <div className="mem5-container">
-      <img src="https://th.bing.com/th/id/R.637e6314b636f28a08577f65550c9f3a?rik=uE%2fIHMZV6KZCpw&riu=http%3a%2f%2fwww.clipartbest.com%2fcliparts%2fyik%2fgAd%2fyikgAdr6T.jpg&ehk=izbxCAjnfmMRHezYkA6F%2fpoKVSZn5TUwV7WVcKsYIN4%3d&risl=&pid=ImgRaw&r=0" alt="example img"/>
+        <img src="https://th.bing.com/th/id/R.4b8ae6534962afba293dca06b718b5df?rik=htS%2bEp2O%2fNwT4Q&riu=http%3a%2f%2fwww.pngmart.com%2ffiles%2f4%2fCute-Cartoon-PNG-Transparent.png&ehk=brf9GkGR2T%2b3zNn5UMkcl4UpZgEauvzVpjGIsvNEzUI%3d&risl=&pid=ImgRaw&r=0" alt="example img"/>
       </div>
       <h4 className="b5">Board Member 5</h4>
 
 
+
+      <div className="boarder6">
+        <div className="decor6"></div>
+        <p className="info6">I like food, dogs, fighting, sometimes cooking, drinking, hang with friends sometimes
+         blah blah blah blah</p>
+      </div> 
       <div className="mem6-container">
-      <img src="https://th.bing.com/th/id/R.637e6314b636f28a08577f65550c9f3a?rik=uE%2fIHMZV6KZCpw&riu=http%3a%2f%2fwww.clipartbest.com%2fcliparts%2fyik%2fgAd%2fyikgAdr6T.jpg&ehk=izbxCAjnfmMRHezYkA6F%2fpoKVSZn5TUwV7WVcKsYIN4%3d&risl=&pid=ImgRaw&r=0" alt="example img"/>
+        <img src="https://th.bing.com/th/id/R.4b8ae6534962afba293dca06b718b5df?rik=htS%2bEp2O%2fNwT4Q&riu=http%3a%2f%2fwww.pngmart.com%2ffiles%2f4%2fCute-Cartoon-PNG-Transparent.png&ehk=brf9GkGR2T%2b3zNn5UMkcl4UpZgEauvzVpjGIsvNEzUI%3d&risl=&pid=ImgRaw&r=0" alt="example img"/>
       </div>
       <h4 className="b6">Board Member 6</h4>
-
-
-      <div className="mem7-container">
-      <img src="https://th.bing.com/th/id/R.637e6314b636f28a08577f65550c9f3a?rik=uE%2fIHMZV6KZCpw&riu=http%3a%2f%2fwww.clipartbest.com%2fcliparts%2fyik%2fgAd%2fyikgAdr6T.jpg&ehk=izbxCAjnfmMRHezYkA6F%2fpoKVSZn5TUwV7WVcKsYIN4%3d&risl=&pid=ImgRaw&r=0" alt="example img"/>
-      </div>
-      <h4 className="b7">Board Member 7</h4>
-
-
-      <div className="mem8-container">
-      <img src="https://th.bing.com/th/id/R.637e6314b636f28a08577f65550c9f3a?rik=uE%2fIHMZV6KZCpw&riu=http%3a%2f%2fwww.clipartbest.com%2fcliparts%2fyik%2fgAd%2fyikgAdr6T.jpg&ehk=izbxCAjnfmMRHezYkA6F%2fpoKVSZn5TUwV7WVcKsYIN4%3d&risl=&pid=ImgRaw&r=0" alt="example img"/>
-      </div>
-      <h4 className="b8">Board Member 8</h4>
-
-
-      <div className="mem9-container">
-      <img src="https://th.bing.com/th/id/R.637e6314b636f28a08577f65550c9f3a?rik=uE%2fIHMZV6KZCpw&riu=http%3a%2f%2fwww.clipartbest.com%2fcliparts%2fyik%2fgAd%2fyikgAdr6T.jpg&ehk=izbxCAjnfmMRHezYkA6F%2fpoKVSZn5TUwV7WVcKsYIN4%3d&risl=&pid=ImgRaw&r=0" alt="example img"/>
-      </div>
-      <h4 className="b9">Board Member 9</h4>
 
 
     </div>
