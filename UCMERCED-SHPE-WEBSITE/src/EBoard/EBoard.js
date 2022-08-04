b746ea42e2df6e6e1e17f929a4d8e8141c33408e
vcortez4 <vcortez4@ucmerced.edu>
2022-08-03 21:54:50 -0700

    almost done

--- UCMERCED-SHPE-WEBSITE/src/EBoard/EBoard.js
+++ UCMERCED-SHPE-WEBSITE/src/EBoard/EBoard.js
@@ -1,96 +1,199 @@
 import "./EBoard.css";
 import React from "react";
 import Navbar from "../Navbar/Navbar";
+import Brandon from "../../src/assets/brandon.jpg";
 
 function EBoard() {
   return (
     <div className="Parent">
 
-      <div className="Title">
-        <h4 className="text"> Executive Board 2022</h4>
+
+      {/* <div className="intro-container">
+        <div className="parent-contain">
+          <h4 className="text"> Executive Board 2022</h4>
+        </div>
+        <img class = "test" src="https://www.mccarthy.com/sites/default/files/project-images/UC%20Merced%20Science%20and%20Engineering%20Building-005.jpg" alt="school"/>
+      </div> */}
+
+      <div className="parent-text">
+        <h1> Executive Board 2022</h1>
       </div>
-
-      <div className="intro-container">
-        <img src="https://www.mccarthy.com/sites/default/files/project-images/UC%20Merced%20Science%20and%20Engineering%20Building-005.jpg" alt="school"/>
+      <div className="banner-container">
+        <img class="banner-image" src="https://www.mccarthy.com/sites/default/files/project-images/UC%20Merced%20Science%20and%20Engineering%20Building-005.jpg" alt="Logo" />
       </div>
-      <div className="m">Cute message</div>
-
-
-
-
 
 
 
       <div className="boarder1">
         <div className="decor1"></div>
-        <p className="info1">I like food, dogs, fighting, sometimes cooking, drinking, hang with friends sometimes
-        blah blah blah blah blah blah blah blah blah blah blah blah blah</p>
+        <h4 className="b1">President</h4>
+        <p className="info1">My name is Brandon and I am very popular. I have 2 precious ass dogs and I like to play video games</p>
       </div>
       <div className="mem1-container">
-          <img src="brandon.jpg" alt="example img"/>
+          <img src={Brandon} alt="example img"></img>
       </div>
-      <h4 className="b1">Board Member 1</h4>
 
 
     
       <div className="boarder2">
         <div className="decor2"></div>
-        <p className="info2">I like food, dogs, fighting, sometimes cooking, drinking, hang with friends sometimes
-         blah blah blah blah</p>
+        <p className="info2">My name is Brandon and I am very popular. I have 2 precious ass dogs and I like to play video games</p>
+        <h4 className="b2">Vice President</h4>
       </div>
       <div className="mem2-container">
-        <img src="https://th.bing.com/th/id/R.4b8ae6534962afba293dca06b718b5df?rik=htS%2bEp2O%2fNwT4Q&riu=http%3a%2f%2fwww.pngmart.com%2ffiles%2f4%2fCute-Cartoon-PNG-Transparent.png&ehk=brf9GkGR2T%2b3zNn5UMkcl4UpZgEauvzVpjGIsvNEzUI%3d&risl=&pid=ImgRaw&r=0" alt="example img"/>
+        <img src={Brandon} alt="example img"></img>
       </div>
-        <h4 className="b2">Board Member 2</h4>
 
 
       <div className="boarder3">
         <div className="decor3"></div>
-        <p className="info3">I like food, dogs, fighting, sometimes cooking, drinking, hang with friends sometimes
-         blah blah blah blah</p>
+        <p className="info3">My name is Brandon and I am very popular. I have 2 precious ass dogs and I like to play video games</p>
+        <h4 className="b3">Treasurer</h4>
       </div>
       <div className="mem3-container">
-        <img src="https://th.bing.com/th/id/R.4b8ae6534962afba293dca06b718b5df?rik=htS%2bEp2O%2fNwT4Q&riu=http%3a%2f%2fwww.pngmart.com%2ffiles%2f4%2fCute-Cartoon-PNG-Transparent.png&ehk=brf9GkGR2T%2b3zNn5UMkcl4UpZgEauvzVpjGIsvNEzUI%3d&risl=&pid=ImgRaw&r=0" alt="example img"/>
+        <img src={Brandon} alt="example img"></img>
       </div>
-        <h4 className="b3">Board Member 3</h4>
 
 
 
 
       <div className="boarder4">
         <div className="decor4"></div>
-        <p className="info4">I like food, dogs, fighting, sometimes cooking, drinking, hang with friends sometimes
-         blah blah blah blah</p>
+        <p className="info4">My name is Brandon and I am very popular. I have 2 precious ass dogs and I like to play video games</p>
+        <h4 className="b4">Secretary</h4>
       </div>    
       <div className="mem4-container">
-        <img src="https://th.bing.com/th/id/R.4b8ae6534962afba293dca06b718b5df?rik=htS%2bEp2O%2fNwT4Q&riu=http%3a%2f%2fwww.pngmart.com%2ffiles%2f4%2fCute-Cartoon-PNG-Transparent.png&ehk=brf9GkGR2T%2b3zNn5UMkcl4UpZgEauvzVpjGIsvNEzUI%3d&risl=&pid=ImgRaw&r=0" alt="example img"/>
+        <img src={Brandon} alt="example img"></img>
       </div>
-        <h4 className="b4">Board Member 4</h4>
-
 
 
 
       <div className="boarder5">
         <div className="decor5"></div>
-        <p className="info5">I like food, dogs, fighting, sometimes cooking, drinking, hang with friends sometimes
-         blah blah blah blah</p>
+        <p className="info5">My name is Brandon and I am very popular. I have 2 precious ass dogs and I like to play video games</p>
+        <h4 className="b5">Webmaster</h4>
       </div> 
       <div className="mem5-container">
-        <img src="https://th.bing.com/th/id/R.4b8ae6534962afba293dca06b718b5df?rik=htS%2bEp2O%2fNwT4Q&riu=http%3a%2f%2fwww.pngmart.com%2ffiles%2f4%2fCute-Cartoon-PNG-Transparent.png&ehk=brf9GkGR2T%2b3zNn5UMkcl4UpZgEauvzVpjGIsvNEzUI%3d&risl=&pid=ImgRaw&r=0" alt="example img"/>
+        <img src={Brandon} alt="example img"></img>
       </div>
-      <h4 className="b5">Board Member 5</h4>
 
 
 
       <div className="boarder6">
         <div className="decor6"></div>
-        <p className="info6">I like food, dogs, fighting, sometimes cooking, drinking, hang with friends sometimes
-         blah blah blah blah</p>
+        <p className="info6">My name is Brandon and I am very popular. I have 2 precious ass dogs and I like to play video games</p>
+        <h4 className="b6">Fundraising chair</h4>
       </div> 
       <div className="mem6-container">
-        <img src="https://th.bing.com/th/id/R.4b8ae6534962afba293dca06b718b5df?rik=htS%2bEp2O%2fNwT4Q&riu=http%3a%2f%2fwww.pngmart.com%2ffiles%2f4%2fCute-Cartoon-PNG-Transparent.png&ehk=brf9GkGR2T%2b3zNn5UMkcl4UpZgEauvzVpjGIsvNEzUI%3d&risl=&pid=ImgRaw&r=0" alt="example img"/>
+        <img src={Brandon} alt="example img"></img>
       </div>
-      <h4 className="b6">Board Member 6</h4>
+
+
+
+
+      <div className="boarder7">
+        <div className="decor7"></div>
+        <p className="info7">My name is Brandon and I am very popular. I have 2 precious ass dogs and I like to play video games</p>
+        <h4 className="b7">Project/Networking Chair</h4>
+      </div>    
+      <div className="mem7-container">
+        <img src={Brandon} alt="example img"></img>
+      </div>
+
+
+
+
+      <div className="boarder8">
+        <div className="decor8"></div>
+        <p className="info8">My name is Brandon and I am very popular. I have 2 precious ass dogs and I like to play video games</p>
+        <h4 className="b8">Outreach Chair</h4>
+      </div> 
+      <div className="mem8-container">
+        <img src={Brandon} alt="example img"></img>
+      </div>
+
+
+
+
+      <div className="boarder9">
+        <div className="decor9"></div>
+        <p className="info9">My name is Brandon and I am very popular. I have 2 precious ass dogs and I like to play video games</p>
+        <h4 className="b9">Manitos Director</h4>
+      </div> 
+      <div className="mem9-container">
+        <img src={Brandon} alt="example img"></img>
+      </div>
+
+
+
+      <div className="boarder10">
+        <div className="decor10"></div>
+        <p className="info10">My name is Brandon and I am very popular. I have 2 precious ass dogs and I like to play video games</p>
+        <h4 className="b10">Internal Representative</h4>
+      </div>    
+      <div className="mem10-container">
+        <img src={Brandon} alt="example img"></img>
+      </div>
+
+
+
+
+      <div className="boarder11">
+        <div className="decor11"></div>
+        <p className="info11">My name is Brandon and I am very popular. I have 2 precious ass dogs and I like to play video games</p>
+        <h4 className="b11">Academic Chair</h4>
+      </div> 
+      <div className="mem11-container">
+        <img src={Brandon} alt="example img"></img>
+      </div>
+
+
+
+
+      <div className="boarder12">
+        <div className="decor12"></div>
+        <p className="info12">My name is Brandon and I am very popular. I have 2 precious ass dogs and I like to play video games</p>
+        <h4 className="b12">Junior Chapter Director</h4>
+      </div> 
+      <div className="mem12-container">
+        <img src={Brandon} alt="example img"></img>
+      </div>
+
+
+      <div className="boarder13">
+        <div className="decor13"></div>
+        <p className="info13">My name is Brandon and I am very popular. I have 2 precious ass dogs and I like to play video games</p>
+        <h4 className="b13">Female Representative</h4>
+
+      </div>    
+      <div className="mem13-container">
+        <img src={Brandon} alt="example img"></img>
+      </div>
+
+
+
+
+      <div className="boarder14">
+        <div className="decor14"></div>
+        <p className="info14">My name is Brandon and I am very popular. I have 2 precious ass dogs and I like to play video games</p>
+        <h4 className="b14">Membership Chair</h4>
+
+      </div> 
+      <div className="mem14-container">
+        <img src={Brandon} alt="example img"></img>
+      </div>
+
+
+
+
+      <div className="boarder15">
+        <div className="decor15"></div>
+        <p className="info15">My name is Brandon and I am very popular. I have 2 precious ass dogs and I like to play video games</p>
+        <h4 className="b15">Vanguard/ICC Representative</h4>
+      </div> 
+      <div className="mem15-container">
+        <img src={Brandon} alt="example img"></img>
+      </div>
 
 
     </div>
